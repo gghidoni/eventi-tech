e5357274a09767e446a572c51ae84c6cebd25641
gghidoni <g.ghidoni3@gmail.com>
2026-09-05 09:21:17 +0200

    chore: archive local container config

---
+++ backend/rector.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        // __DIR__ . '/bootstrap',
+        // __DIR__ . '/config',
+        // __DIR__ . '/public',
+        // __DIR__ . '/resources',
+        __DIR__.'/routes',
+        // __DIR__ . '/tests',
+    ])
+    // ->withSkip([AddOverrideAttributeToOverriddenMethodsRector])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    );
+// ->withTypeCoverageLevel(0)
+// ->withDeadCodeLevel(0)
+// ->withCodeQualityLevel(0);
